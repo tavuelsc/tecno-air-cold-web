9e5ed61acf84b638f55b1a22a4dc8b34b632a800
jicv22 <jicv8@outlook.com>
2025-05-06 09:01:19 -0600

    Navbar, placeholder components and basic structure distribution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './sections/home/home.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { ReviewsComponent } from './sections/reviews/reviews.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { TeamComponent } from './sections/team/team.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent,
+    ServicesComponent,
+    ReviewsComponent,
+    ExperienceComponent,
+    TeamComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tecno-air-cold-web';
