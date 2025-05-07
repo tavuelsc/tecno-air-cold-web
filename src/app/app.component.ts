186fa8768f1339ec827fcbf4c454594a7a690aa3
jicv22 <jicv8@outlook.com>
2025-05-07 08:45:57 -0600

    Basic navbar and first section home responsives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { ReviewsComponent } from './sections/reviews/reviews.component';
 import { ExperienceComponent } from './sections/experience/experience.component';
 import { TeamComponent } from './sections/team/team.component';
 import { ContactComponent } from './sections/contact/contact.component';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -25,4 +27,52 @@ import { ContactComponent } from './sections/contact/contact.component';
 })
 export class AppComponent {
   title = 'tecno-air-cold-web';
+
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'logo-tecno-air',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/logos/logo-tecno-air-cold-logo.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'house',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-house.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'tools',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-tools.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'message-bubble',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-message-bubble.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'briefcase',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-briefcase.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'persons-with-star',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-persons-with-star.svg'
+      )
+    );
+    this.iconRegistry.addSvgIcon(
+      'person-identification',
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/images/icons/icon-person-identification.svg'
+      )
+    );
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 
 export class NavbarComponent {
-  
+
+  menuOpen = false;
+  isScrolled = false;
+  activeSection: string = '';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isScrolled = offset > 100;
+
+    const sectionIds = ['inicio', 'servicios', 'resenas', 'experiencia', 'equipo', 'contacto'];
+    for (let id of sectionIds) {
+      const section = document.getElementById(id);
+      if (section) {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 180 && rect.bottom >= 180) {
+          this.activeSection = id;
+          break;
+        }
+      }
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
 }
